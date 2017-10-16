3b948147e215afba425458ec490330c5a6ae54b7
fujisawakyle <fujisawakyle@gmail.com>
2017-10-15 17:48:14 -0700

    fix entry input

--- client/src/components/reflection/Entry.js
+++ client/src/components/reflection/Entry.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
-import * as actions from '../../actions';
 
-import EntryField from './EntryField';
-
+//this grabs input from props // es6
 class Entry extends Component {
-  update = entry => {
-    this.props.updateEntry(this.props.selectedDay);
+  state = {
+    entry: this.props.selectedMediflection.entry
   };
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({ entry: nextProps.selectedMediflection.entry });
+  }
+
+  handleChange = event => {
+    this.setState({
+      entry: event.target.value
+    });
+  };
+  update = entry => {
+    console.log(entry);
+  };
   render() {
+    console.log('in Entry', this.props);
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(this.update)}>
-          <Field name="entry" component={EntryField} />
-          <button type="submit">Submit</button>
-        </form>
+        <textarea
+          autoComplete="off"
+          onChange={this.handleChange}
+          value={this.state.entry}
+          placeholder="Today's meditation was:"
+        />
+        <button onClick={() => this.update(this.state.entry)} type="submit">
+          Submit
+        </button>
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps({ selectedMediflection }) {
   return {
-    formValues: state.form.entryForm
+    selectedMediflection
   };
 }
 
-Entry = reduxForm({
-  form: 'entryForm',
-  enableReinitialize: true
-})(Entry);
-
-Entry = connect(mapStateToProps, actions)(Entry);
+Entry = connect(mapStateToProps)(Entry);
 
 export default Entry;
